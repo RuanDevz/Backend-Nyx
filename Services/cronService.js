654f8040f01475805920a4bff16c8d33c5660e62
RuanDevz <Ruanbatista1509@outlook.com>
2025-01-29 00:46:27 -0300

    webhook stripe

--- Services/cronService.js
+++ Services/cronService.js
@@ -1,5 +1,8 @@
-const { User } = require('../models');
+// Services/cronService.js
+const cron = require('node-cron');
+const { User, Op } = require('../models'); // Importando o Op para usá-lo no filtro
 
+// Função para verificar a expiração do VIP
 async function checkVipExpiration() {
     try {
         const users = await User.findAll({
@@ -11,13 +14,20 @@ async function checkVipExpiration() {
             },
         });
 
+        // Se houver usuários com VIP expirado, atualiza a flag
         for (const user of users) {
             await user.update({ isVip: false });
+            console.log(`Usuário ${user.id} teve seu VIP desativado.`);
         }
     } catch (error) {
         console.error('Erro ao verificar expiração do VIP:', error.message, error.stack);
     }
 }
 
-// Executa a verificação a cada 24 horas
-setInterval(checkVipExpiration, 24 * 60 * 60 * 1000);
+// Função para iniciar o cron job
+function startCronJob() {
+    cron.schedule('0 0 * * *', checkVipExpiration); // Executa à meia-noite todos os dias
+    console.log('Cron job iniciado.');
+}
+
+module.exports = startCronJob; 
