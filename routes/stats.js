41394df48f4e9e13e6a3c3bb62201a252a2677d3
RuanDevz <Ruanbatista1509@outlook.com>
2025-04-01 02:16:39 -0300

    alguns commits removidos

--- routes/stats.js
+++ routes/stats.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../models"); // Importando o modelo de User
+const { User } = require("../models"); 
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
